b86c5228368e51cbf7a4271618efdb476e3a4df6
vladanan <96662847+vladanan@users.noreply.github.com>
2024-03-08 11:50:24 +0100

    Create v1.blade.php

---
+++ resources/views/v1.blade.php
@@ -0,0 +1,21 @@
+<html lang="en">
+
+  <head>
+    <title>Programiramo</title>
+    <style></style>
+  </head>
+
+  <body>
+    <h1>Programiramo</h1>
+    
+    <a href="srp/o1/_o1.php">o1</a><br>
+    <a href="srp/s1/_s1.php">s1</a>
+
+    
+    
+    <script>
+    
+    </script>
+
+  </body>
+</html>
